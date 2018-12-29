d42414759f2423f3dba6716b6f0be5439cbf5801
CesarRivasP <cesarjose58@gmail.com>
2018-12-29 12:53:18 -0400

    Adding new reducer 'city'

---
+++ src/reducers/city.js
@@ -0,0 +1,21 @@
+import { SET_CITY } from '../actions'
+
+export const city = (state, action) => {
+  switch (action.type){
+    //para detectar cuando el action tenga un type que sea igual a city, el reducer responde modificando el estado.
+    case SET_CITY: //Cuando se genere una accion de este tipo, se retorna:
+      return {...state, city: action.value};
+      /*
+        Un nuevo estado que se va a conformar por el estado anterior, mas el valor de la accion.
+        Asi se obtiene el nuevo objeto que se necesita en este punto
+      */
+  }
+  return state;
+}
+
+
+
+/*
+Este reducer nos va a permitir manejar la ciudad activa.
+
+*/
