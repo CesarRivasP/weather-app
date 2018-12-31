9c9ea521f5885c211a4e3254002781fe59527e4e
CesarRivasP <cesarjose58@gmail.com>
2018-12-31 11:16:06 -0400

    Added case "default" case and change value for payload. Also default param for the reducers function

--- src/reducers/city.js
+++ src/reducers/city.js
@@ -1,14 +1,16 @@
 import { SET_CITY } from '../actions'
 
-export const city = (state, action) => {
+export const city = (state = {}, action) => {
   switch (action.type){
     //para detectar cuando el action tenga un type que sea igual a city, el reducer responde modificando el estado.
     case SET_CITY: //Cuando se genere una accion de este tipo, se retorna:
-      return {...state, city: action.value};
+      return { ...state, city: action.payload };
       /*
         Un nuevo estado que se va a conformar por el estado anterior, mas el valor de la accion.
         Asi se obtiene el nuevo objeto que se necesita en este punto
       */
+    default:
+      return state;
   }
   return state;
 }
