9916bd6d0b7193c9b582f121aad936d7c3362275
CesarRivasP <cesarjose58@gmail.com>
2018-12-31 11:16:32 -0400

    Change value for payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
-
-
 export const SET_CITY = 'SET_CITY';
 
-export const setCity = (value) => ({ type: SET_CITY, value });
+export const setCity = (payload) => ({ type: SET_CITY, payload });
+
+// Por convencion, en vez de de 'value' se denomina 'payload'.
