0793c8aff05350099c85f5234e76c16a8810cc2d
CesarRivasP <cesarjose58@gmail.com>
2019-01-03 12:08:03 -0400

    redux thunk implementation

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { city } from '../reducers/city';
 
 const initialState = {
@@ -8,14 +9,22 @@ const initialState = {
 // const reducer = (state, action) => {
 //   return state; //devuelve exactamente el mismo state que recibe como parametro y no hay modificacion del estado
 // }
-                            /* after ()=> {}*/
-export const store = createStore(/*reducer*/city, initialState, //Generar el store
- window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //Instalación del devtools
-);
 
+// BEFORE STORE                  before ()=> {}
+// export const store = createStore(/*reducer*/city, initialState, //Generar el store
+//  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //Instalación del devtools
+// );
 
 /*
 Como aqui se pasa por parametro del reducer 'city', si se le cambia el nombre a lo que retorna el reductor,
 no cambia el estado, por lo que el nombre del producto del reductor debe ser  similar al que se le esta pasando
 al store.
 */
+
+//AFTER STORE
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
+                                                              //aqui se pasa el Middlware que se esta usando 'thunk'
+export const store = createStore(city, initialState, composeEnhancers(applyMiddleware(thunk)));
+
+
+//En caso de no usar el devtools, se puede prescindir del composeEnhancers y quedaria -> applyMiddleware(thunk)
