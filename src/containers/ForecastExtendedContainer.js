8a76e81e35300c391e2d5695542e9bd8318d2299
CesarRivasP <cesarjose58@gmail.com>
2019-01-05 14:32:15 -0400

    Added connection with the state for forecastdata

--- src/containers/ForecastExtendedContainer.js
+++ src/containers/ForecastExtendedContainer.js
@@ -1,26 +1,34 @@
 import React, {Component} from 'react';
 import ForecastExtended from '../components/ForecastExtended'
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 class ForecastExtendedContainer extends Component {
 
   render() {
-    const { city } = this.props;
+    const { city, forecastData } = this.props;
 
     return (
       city &&
-        <ForecastExtended city={city}/>
+        <ForecastExtended city={ city } forecastData={ forecastData } />
     )
   }
 }
 
 ForecastExtendedContainer.propTypes = {
-  city: PropTypes.string.isRequired
+  city: PropTypes.string.isRequired,
+  forecastData: PropTypes.array,
 }
 
 // const mapStateToProps = (state) => ({ city: state.city });
-const mapStateToProps = ({city}) => ({city}); //Simplificado mediante destructuring
+const mapStateToProps = ({ city, cities }) => (
+  {
+    city,
+    // buscar en cities[en la ciudad seleccionada]. el forecastData
+    forecastData: cities[city] && cities[city].forecastData, // <- para que del estado global de la app nos venga esta info
+    //De no hacer la validacion, da error porque en un inicio city es null
+  }
+); //Simplificado mediante destructuring
 //toma la propiedad city que esta dentro de state, luego se asigna -- city = city  === city
 
 export default connect(mapStateToProps, null)(ForecastExtendedContainer);
