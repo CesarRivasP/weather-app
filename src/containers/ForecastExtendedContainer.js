57a3f3a0cdfbd05259f5a24af948802a322c4529
CesarRivasP <cesarjose58@gmail.com>
2019-01-05 15:11:08 -0400

    state abstraction with selector getForecastDataFromCities

--- src/containers/ForecastExtendedContainer.js
+++ src/containers/ForecastExtendedContainer.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ForecastExtended from '../components/ForecastExtended'
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { getForecastDataFromCities } from '../reducers/cities';
 
 class ForecastExtendedContainer extends Component {
 
@@ -21,14 +22,27 @@ ForecastExtendedContainer.propTypes = {
 }
 
 // const mapStateToProps = (state) => ({ city: state.city });
-const mapStateToProps = ({ city, cities }) => (
+//const mapStateToProps = ({ city, cities }) => (
+const mapStateToProps = state => (  //state para no tener conocimiento (sea visible) de lo que contiene state
   {
-    city,
+    //city before
+    city: state.city, //after
     // buscar en cities[en la ciudad seleccionada]. el forecastData
-    forecastData: cities[city] && cities[city].forecastData, // <- para que del estado global de la app nos venga esta info
+    // before ->forecastData: cities[city] && cities[city].forecastData, // <- para que del estado global de la app nos venga esta info
     //De no hacer la validacion, da error porque en un inicio city es null
+
+    // En este container, necesitamos solo la parte del estado que tiene forecastData para la ciudad que se selecciono.
+    // Este container tiene un amplio conocimiento con respecto al estado global de la app, a la estructura del estado bastante importante.
+
+    //Generacion de un selector en el reducer de cities, puesto que es el lugar en donde se conoce el estado, su estructura.
+    //Por eso es el punto mas adecuado para establecer un selector
+    //AFTER
+    forecastData: getForecastDataFromCities(state.cities, state.city), //abstraccion del estado de la app mediante selectors
   }
 ); //Simplificado mediante destructuring
 //toma la propiedad city que esta dentro de state, luego se asigna -- city = city  === city
 
 export default connect(mapStateToProps, null)(ForecastExtendedContainer);
+
+/*
+Dentro de cities hay un diccionario 'cities[city]'. De ese diccionario se toma una entrada, y de ahi se llega al forecastData */
