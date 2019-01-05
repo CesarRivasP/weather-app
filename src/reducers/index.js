a9b9ee6d34e3435c25c874d4afb2277e03480232
CesarRivasP <cesarjose58@gmail.com>
2019-01-05 18:44:44 -0400

    Added selectors functions getCity and getForecastDataFromCities

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,24 @@
 import { combineReducers } from 'redux';
-import { cities } from './cities';
+import {
+  cities,
+  getForecastDataFromCities as _getForecastDataFromCities,
+ } from './cities';
 import { city } from './city';
 
 export default combineReducers({
 //reducer's
   cities,
   city
-})
+});
+
+export const getCity = (state) => (state.city); //Other selector
+                              //recibe el state global => llama a la funcion original, y a esta se le pasa state.cities, state.city
+export const getForecastDataFromCities = (state) => (_getForecastDataFromCities(state.cities, getCity(state)/*state.city*/));
+
+
+/*
+Al importar getForecastDataFromCities desde cities la funcion original con un alias para poder reutlizarla con el mismo nombre.
+El alias _getForecastDataFromCities nos permite llamar desde 'export ..getForecastDataFromCities' a la funcion original.
+Con este refactor se logra mayor abstraccion de donde se obtiene el dato forecastData.
+EL refactor de state.city es para no tener distintos puntos donde se conoce como esta conformado la estructura del reducer de city
+*/
