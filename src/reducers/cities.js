8d4533c08ade2dfff2195a4940f00537c1f4c0e6
CesarRivasP <cesarjose58@gmail.com>
2019-01-05 15:10:16 -0400

    Added notes about selectors in cities reducers and cities selector getForecastDataFromCities

--- src/reducers/cities.js
+++ src/reducers/cities.js
@@ -11,7 +11,17 @@ export const cities = (state = {}, action) => {
   }
 }
 
+
+//selector                          state,citySelected => state que maneja cities (el estate que maneja el reducer anterior y no al state global de la app)
+export const getForecastDataFromCities = (state, city) => state[city] && state[city].forecastData   //si es diferente de null
+//Este selector obtiene el forecastData para la ciudad seleccionada
+
+//Obtener la informacion del forecast (pronostico extendido) desde las ciudades.
+
 /*
 [city]: {forecastData: forecastData, weather: null}   //el nombre de la ciudad va a ser una clave
 ESta es la primera entrada del diccionario correspondiente a la ciudad que se seleccione
+
+- state[city] state de la ciudad seleccionada
+- ese state tiene el diccionario (cities) donde estan todas las ciudades visitadas anteriormente
 */
