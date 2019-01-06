1b6f2a59b2c259ad566b452ccbb8ad130b128efe
CesarRivasP <cesarjose58@gmail.com>
2019-01-06 17:31:05 -0400

    Implementation of reselector library in cities reducers

--- src/reducers/cities.js
+++ src/reducers/cities.js
@@ -1,3 +1,4 @@
+import { createSelector } from 'reselect';
 import { SET_FORECAST_DATA } from '../actions';
 
 export const cities = (state = {}, action) => {
@@ -12,8 +13,8 @@ export const cities = (state = {}, action) => {
 }
 
 
-//selector                          state,citySelected => state que maneja cities (el estate que maneja el reducer anterior y no al state global de la app)
-export const getForecastDataFromCities = (state, city) => state[city] && state[city].forecastData   //si es diferente de null
+//selector -before                        state,citySelected => state que maneja cities (el estate que maneja el reducer anterior y no al state global de la app)
+//export const getForecastDataFromCities = (state, city) => state[city] && state[city].forecastData   //si es diferente de null
 //Este selector obtiene el forecastData para la ciudad seleccionada
 
 //Obtener la informacion del forecast (pronostico extendido) desde las ciudades.
@@ -25,3 +26,9 @@ ESta es la primera entrada del diccionario correspondiente a la ciudad que se se
 - state[city] state de la ciudad seleccionada
 - ese state tiene el diccionario (cities) donde estan todas las ciudades visitadas anteriormente
 */
+
+
+//after
+export const getForecastDataFromCities =
+  createSelector((state, city) => state[city] && state[city].forecastData, forecastData => forecastData);
+//obtiene el forecastData y despues se le indica que eso sea el resultado final
