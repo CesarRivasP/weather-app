71c9563a27a8d391dec3dcde1af0223a5fd687b6
CesarRivasP <cesarjose58@gmail.com>
2019-01-06 17:32:08 -0400

    implementation of reselector library in reducers index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
-import {
-  cities,
-  getForecastDataFromCities as _getForecastDataFromCities,
- } from './cities';
+import { createSelector } from 'reselect';
+import { cities, getForecastDataFromCities as _getForecastDataFromCities } from './cities';
 import { city } from './city';
 
 export default combineReducers({
@@ -10,11 +8,10 @@ export default combineReducers({
   cities,
   city
 });
-
-export const getCity = (state) => (state.city); //Other selector
+//before
+// export const getCity = (state) => (state.city); //Other selector
                               //recibe el state global => llama a la funcion original, y a esta se le pasa state.cities, state.city
-export const getForecastDataFromCities = (state) => (_getForecastDataFromCities(state.cities, getCity(state)/*state.city*/));
-
+// export const getForecastDataFromCities = (state) => (_getForecastDataFromCities(state.cities, getCity(state)/*state.city*/));
 
 /*
 Al importar getForecastDataFromCities desde cities la funcion original con un alias para poder reutlizarla con el mismo nombre.
@@ -22,3 +19,14 @@ El alias _getForecastDataFromCities nos permite llamar desde 'export ..getForeca
 Con este refactor se logra mayor abstraccion de donde se obtiene el dato forecastData.
 EL refactor de state.city es para no tener distintos puntos donde se conoce como esta conformado la estructura del reducer de city
 */
+
+//AFTER
+export const getCity = createSelector(state => state.city, city => city);
+//createSelector(funcion como un selector, funcion resultante )
+//La resultfunc va a recibir como parametro lo que arroje el anterior selector. Va a recibir 'city' => para obtener 'city'
+                                              //del state => toma solo la cities,
+export const getForecastDataFromCities = createSelector(state => state.cities, getCity, _getForecastDataFromCities);
+//Antes en getCity se le pasaba el state, y se obtenia la ciudad para pasarla como parametro a _getForecastDataFromCities
+//Como el state lo esta invocando directamente la libreria mediante el createSelector, solo hay que pasarle el selector 'getCity'
+//Se tienen dos funciones que retornan dos resultados. La resultfunc va a tener como parametro a (cities, city) y con eso se obtiene la porcion
+//del estado que se requiere.
