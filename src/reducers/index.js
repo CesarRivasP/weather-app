885c25525fe125eedbf959dbc53f2c55ce80e3bd
CesarRivasP <cesarjose58@gmail.com>
2019-01-06 23:48:09 -0400

    Added implementation of new selector getWeatherCities

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import { createSelector } from 'reselect';
-import { cities, getForecastDataFromCities as _getForecastDataFromCities } from './cities';
+import { cities,
+  getForecastDataFromCities as _getForecastDataFromCities,
+  getWeatherCities as _getWeatherCities
+ } from './cities';
 import { city } from './city';
 
 export default combineReducers({
@@ -25,8 +28,11 @@ export const getCity = createSelector(state => state.city, city => city);
 //createSelector(funcion como un selector, funcion resultante )
 //La resultfunc va a recibir como parametro lo que arroje el anterior selector. Va a recibir 'city' => para obtener 'city'
                                               //del state => toma solo la cities,
-export const getForecastDataFromCities = createSelector(state => state.cities, getCity, _getForecastDataFromCities);
+export const getForecastDataFromCities =
+  createSelector(state => state.cities, getCity, _getForecastDataFromCities);
 //Antes en getCity se le pasaba el state, y se obtenia la ciudad para pasarla como parametro a _getForecastDataFromCities
 //Como el state lo esta invocando directamente la libreria mediante el createSelector, solo hay que pasarle el selector 'getCity'
 //Se tienen dos funciones que retornan dos resultados. La resultfunc va a tener como parametro a (cities, city) y con eso se obtiene la porcion
 //del estado que se requiere.
+
+export const getWeatherCities = createSelector(state => state.cities, _getWeatherCities);
