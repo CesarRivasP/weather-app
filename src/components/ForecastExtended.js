1753b7c9f13db93921a1fd5ca6d6a4fc5673428a
CesarRivasP <cesarjose58@gmail.com>
2019-01-10 12:56:33 -0400

    Commented service without using

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ForecastItem from './ForecastItem/';
-import transformForecast from '../services/transformForecast';
+// import transformForecast from '../services/transformForecast';
 import './styles.css';
 
 
