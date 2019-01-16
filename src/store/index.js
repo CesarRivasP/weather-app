345bf512defe54f12e7739765a03d38e4b46b79f
CesarRivasP <cesarjose58@gmail.com>
2019-01-15 22:07:01 -0400

    cleaning code of the store

--- src/store/index.js
+++ src/store/index.js
@@ -7,20 +7,10 @@ const initialState = {
   city: 'Caracas,ve'
 };
 
-// const reducer = (state, action) => {
-//   return state; //devuelve exactamente el mismo state que recibe como parametro y no hay modificacion del estado
-// }
-
 // BEFORE STORE                  before ()=> {}
 // export const store = createStore(/*reducer*/city, initialState, //Generar el store
 //  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //Instalación del devtools
 // );
-
-/*
-Como aqui se pasa por parametro del reducer 'city', si se le cambia el nombre a lo que retorna el reductor,
-no cambia el estado, por lo que el nombre del producto del reductor debe ser  similar al que se le esta pasando
-al store.
-*/
 
 //AFTER STORE
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
