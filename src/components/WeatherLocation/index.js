106cdfd91d3137540a93f6025dac1e27137705fd
CesarRivasP <cesarjose58@gmail.com>
2019-01-15 22:06:44 -0400

    cleaning code in weatherLocation index to component

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -5,56 +5,9 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
-// import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-// import transformWeather from '../../services/transformWeather';
-// import getUrlWeatherByCity from '../../services/getUrlWeatherByCity';
 
 
-// class WeatherLocation extends Component {
 const WeatherLocation = ({ onWeatherLocationClick, city, data }) => (
-  // constructor(props){
-  //   super(props);
-  //   const { city } = props;
-  //   this.state = {
-  //     city,
-  //     data: null,
-  //   };
-  //   console.log("Constructor");
-  // }
-
-  // componentDidMount(){
-  //   console.log("componentDidMount");
-  //   this.handleUpdateClick();
-  // }
-
-  // componentDidUpdate(prevProps, prevState){
-  //   console.log("componentDidUpdate");
-  // }
-
-  // handleUpdateClick = () => {
-  //   const api_weather = getUrlWeatherByCity(this.state.city);
-  //
-  //   fetch(api_weather).then(resolve => {
-  //       return resolve.json();
-  //       //debugger;
-  //   }).then(data => {
-  //     console.log(data)
-  //     // debugger;
-  //     console.log("resolve handleUpdateClick");
-  //     const newWeather = transformWeather(data);
-  //     console.log(newWeather)
-  //     //debugger;
-  //     this.setState({
-  //       data: newWeather
-  //     })
-  //   })
-  //   console.log("Actualizado");
-  // }
-  //render()
-    // const { city, data } = this.state;
-    // const { onWeatherLocationClick, city, data } = this.props;
-
-  // return(
   <div className="weatherLocationCont" onClick={onWeatherLocationClick}>
    <Location
      city={ city }/>
