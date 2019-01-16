d6bfb2190e9d917ded81dcb13bc00dee6e663adb
CesarRivasP <cesarjose58@gmail.com>
2019-01-15 22:05:29 -0400

    cleaning code of the actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,7 +11,7 @@ export const SET_WEATHER_CITY = 'SET_WEATHER_CITY';
 const api_key = "2655d1ef49563a6f6be6cd4971b05085";
 const url = "https://api.openweathermap.org/data/2.5/forecast";
 
-// export const setCity = (payload) => ({ type: SET_CITY, payload });
+
 const setCity = (payload) => ({ type: SET_CITY, payload });
 
 const setForecastData = (payload) => ({ type: SET_FORECAST_DATA, payload });
@@ -19,16 +19,10 @@ const setForecastData = (payload) => ({ type: SET_FORECAST_DATA, payload });
 //Establece la ciudad actual y a la vez realiza la peticion al server del pronostico extendido
 export const setSelectedCity = (payload) => {
   //se declara asi gracias al middlware
-  // return dispatch => {  //aqui se genera una funcion que tiene como parametro 'dispatch'
-  //Para agregar la validacion de la peticion con respecto al tiempo
-  //Hay que acceder al state de la app, por lo que el segundo parametro del redux thunk es el getState
-  //getState es una funcion que obtiene el estado global de la aplicacion
   return (dispatch, getState) => {
 
    const url_forecast= `${url}?q=${payload}&appid=${api_key}`;
 
-    // accion inicial -> para establecer que se esta ajecutando una busqueda
-    // por lo que se va a activar en el estado un indicador de busqueda de datos
     dispatch(setCity(payload));  //ciudad que el usuario selecciono, establece la ciudad actial
 
     const state = getState();
@@ -44,7 +38,6 @@ export const setSelectedCity = (payload) => {
       .then(data => (data.json()))
       .then(weather_data => {
          const forecastData = transformForecast(weather_data)
-          //modificar el estado con el resultado de la promise (http request== fetch)
           //el payload lleva la city de la data que se esta consultando
           // forecastData lleva la informacion del pronostico extendido de la city
           dispatch(setForecastData({ city: payload, forecastData}));
@@ -82,7 +75,5 @@ const gettWeatherCity = (payload) => ({ type: GET_WEATHER_CITY, payload });
 
 //Con la info que retorne el server, lo va a establecer en el estado global
 const setWeatherCity = (payload) => ({ type: SET_WEATHER_CITY, payload });
-
-
 //setWeather internamente va a estar estableciendo los datos dentro del array cities, para luego pasarlos a locationList
 // Por convencion, en vez de de 'value' se denomina 'payload'.
