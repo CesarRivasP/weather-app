b54e9f14e7e4a8a4ea691748473f1e4c22e8ee70
CesarRivasP <cesarjose58@gmail.com>
2019-01-20 22:17:35 -0400

    others changes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,18 +6,21 @@ import { cities,
  } from './cities';
 import { city } from './city';
 
+
 export default combineReducers({
 //reducer's
   cities,
   city
 });
 
-//AFTER
-export const getCity = createSelector(state => state.city, city => city);
-//createSelector(funcion como un selector, funcion resultante )
-//La resultfunc va a recibir como parametro lo que arroje el anterior selector. Va a recibir 'city' => para obtener 'city'
-                                              //del state => toma solo la cities,
-export const getForecastDataFromCities =
-  createSelector(state => state.cities, getCity, _getForecastDataFromCities);
+//AFTER - createSelector(funcion como un selector, funcion resultante )
+export const getCity = createSelector(
+  //La resultfunc va a recibir como parametro lo que arroje el anterior selector. Va a recibir 'city' => para obtener 'city'
+  (state) => state.city, city => city   //del state => toma solo la cities,
+);
+
+export const getForecastDataFromCities = createSelector(
+  (state) => state.cities, getCity, _getForecastDataFromCities
+);
 
 export const getWeatherCities = createSelector(state => state.cities, _getWeatherCities);
