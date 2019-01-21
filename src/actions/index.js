2990cb5d9944b70d37343cc60e9286cf73a7a218
CesarRivasP <cesarjose58@gmail.com>
2019-01-20 22:12:37 -0400

    Move api routes to other file and others changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,13 @@
 import transformForecast from '../services/transformForecast'
 import transformWeather from '../services/transformWeather';
 import getUrlWeatherByCity from '../services/getUrlWeatherByCity';
+import { URL, API_KEY} from '../api/url';
 export const SET_CITY = 'SET_CITY';
 export const SET_FORECAST_DATA = 'SET_FORECAST_DATA';
 export const SET_WEATHER = 'SET_WEATHER';
 export const GET_WEATHER_CITY = 'GET_WEATHER_CITY';
 export const SET_WEATHER_CITY = 'SET_WEATHER_CITY';
 
-
-const api_key = "2655d1ef49563a6f6be6cd4971b05085";
-const url = "https://api.openweathermap.org/data/2.5/forecast";
 
 
 const setCity = (payload) => ({ type: SET_CITY, payload });
@@ -18,10 +16,9 @@ const setForecastData = (payload) => ({ type: SET_FORECAST_DATA, payload });
 
 //Establece la ciudad actual y a la vez realiza la peticion al server del pronostico extendido
 export const setSelectedCity = (payload) => {
-  //se declara asi gracias al middlware
   return (dispatch, getState) => {
 
-   const url_forecast= `${url}?q=${payload}&appid=${api_key}`;
+   const url_forecast= `${URL}?q=${payload}&appid=${API_KEY}`;
 
     dispatch(setCity(payload));  //ciudad que el usuario selecciono, establece la ciudad actial
 
@@ -29,14 +26,15 @@ export const setSelectedCity = (payload) => {
     const date = state.cities[payload] && state.cities[payload].forecastDataDate;
     const now = new Date();
 
-    if(date && (now - date) < 1* 60 * 1000){ //la diferencia viene en milisegundos alo multiplicar x 1000
+    if(date && (now - date) < 1* 60 * 1000){
+      //la diferencia viene en milisegundos alo multiplicar x 1000
       //Si hace menos de un minuto se solicito el forescastData, no volvera a hacer la peticion
       return;
     }
 
     return fetch(url_forecast)
-      .then(data => (data.json()))
-      .then(weather_data => {
+      .then((data) => (data.json()))
+      .then((weather_data) => {
          const forecastData = transformForecast(weather_data)
           //el payload lleva la city de la data que se esta consultando
           // forecastData lleva la informacion del pronostico extendido de la city
@@ -51,17 +49,17 @@ export const setSelectedCity = (payload) => {
 export const setWeather = (payload) => {
   //EN payload llega un array con todas las ciudades
   return dispatch => {
-    payload.forEach(city => {
+    payload.forEach((city) => {
       //se va a ejecutar por cada una de las ciudades a medida que se ejecuta el foreach
       dispatch(gettWeatherCity(city));
 
       const api_weather = getUrlWeatherByCity(city);
 
       fetch(api_weather)
-        .then(resolve => {
+        .then((resolve) => {
             return resolve.json();
         })
-        .then(weather_data => {
+        .then((weather_data) => {
           const weather = transformWeather(weather_data);
 
           dispatch(setWeatherCity({ city, weather }));
@@ -75,5 +73,5 @@ const gettWeatherCity = (payload) => ({ type: GET_WEATHER_CITY, payload });
 
 //Con la info que retorne el server, lo va a establecer en el estado global
 const setWeatherCity = (payload) => ({ type: SET_WEATHER_CITY, payload });
-//setWeather internamente va a estar estableciendo los datos dentro del array cities, para luego pasarlos a locationList
-// Por convencion, en vez de de 'value' se denomina 'payload'.
+/*setWeather internamente va a estar estableciendo los datos dentro del array cities,
+para luego pasarlos a locationList */
