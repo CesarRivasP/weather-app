b09896f1a1b994573f5587c93f058e6e8174eb27
CesarRivasP <cesarjose58@gmail.com>
2019-01-20 22:17:44 -0400

    others changes

--- src/services/getUrlWeatherByCity.js
+++ src/services/getUrlWeatherByCity.js
@@ -2,7 +2,7 @@ import { url_base_weather, api_key } from '../constants/api_url'
 
 
 const getUrlWeatherByCity = (city) => {
-  return `${url_base_weather}?q=${city}&appid=${api_key}`
+  return `${url_base_weather}?q=${city}&appid=${api_key}`;
 }
 
 export default getUrlWeatherByCity;
