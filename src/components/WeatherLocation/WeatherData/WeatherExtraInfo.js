709daf2bc3753531822bec127863e6afb888489c
CesarRivasP <cesarjose58@gmail.com>
2019-01-20 22:14:10 -0400

    other change

--- src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
+++ src/components/WeatherLocation/WeatherData/WeatherExtraInfo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './styles.css';
 
-const WeatherExtraInfo = ({humidity, wind}) => (
+const WeatherExtraInfo = ({ humidity, wind }) => (
   <div className="weatherExtraInfoCont">
     <span className="extraInfoText">{`Humedad: ${humidity} %`}</span>
     <span className="extraInfoText">{`Vientos: ${wind}`}</span>
